f278d76eeb7fc819c6219fc25ffb4176c7dc59ba
Abdula Ahmed <ahmed.abdula.hslc@gmail.com>
2024-12-06 09:24:25 -0500

    first commit

--- models/fruits.js
+++ models/fruits.js
@@ -1,12 +1,15 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const fruitSchema = new mongoose.Schema({
-    name: {type: String, required: true},
-    color: {type: String, required: true},
-    readyToEat: Boolean
-}, {
-    timestamps: true
-})
+const fruitSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    color: { type: String, required: true },
+    readyToEat: Boolean,
+  },
+  {
+    timestamps: true,
+  }
+);
 
 const Fruit = mongoose.model("Fruit", fruitSchema);
 
